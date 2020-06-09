dca163a8e41866c574a26ee1ffbc9d65463e8c4b
alish madhvani <madhvana@sheridancollege.ca>
2020-06-08 21:02:15 -0400

    logic done

--- public_html/js/javascript.js
+++ public_html/js/javascript.js
@@ -3,23 +3,45 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var x=2;
-
+var x=1;
+var y=0;
 function func1(){
-        document.getElementById("content").style.visibility="hidden";
-    if(x==2){
+    if(document.getElementById("ra").checked){
+        document.getElementById("content").style.display="none";
+         document.getElementById("results1").style.display="block";
+         y=y+1
+    }
+    else{
+        document.getElementById("results2").style.display="block";
+         document.getElementById("content").style.display="none";
+    }    
+}
+
+function func2(){
+     document.getElementById("results1").style.display="none";
+      document.getElementById("results2").style.display="none";
+    x=x+1
+    document.getElementById("content").style.display="block";
+     if(x==2){
+         
       document.getElementById("leg").innerHTML="Q2. What was the name of Iron Man in MCU?"  ;
-      document.getElementById("wa1").innerHTML="pony stark";
-      document.getElementById("ra").innerHTML="tony stark";
-      document.getElementById("wa2").innerHTML="lony stark";
-      document.getElementById("wa3").innerHTML="grony stark";
-      x=x+1;
+      document.getElementById("opt1").innerHTML="pony stark";
+      document.getElementById("opt2").innerHTML="tony stark";
+      document.getElementById("opt3").innerHTML="lony stark";
+      document.getElementById("opt4").innerHTML="grony stark";
     }   
+    
     if(x==3){
         document.getElementById("leg").innerHTML="Q3. 1 second is how many millisecond ?"  ;
-      document.getElementById("wa1").innerHTML="100";
-      document.getElementById("ra").innerHTML="1000";
-      document.getElementById("wa2").innerHTML="10";
-      document.getElementById("wa3").innerHTML="10000";
+      document.getElementById("opt1").innerHTML="100";     
+      document.getElementById("opt2").innerHTML="1000";
+      document.getElementById("opt3").innerHTML="10";
+      document.getElementById("opt4").innerHTML="10000";
+    }
+    
+    if(x==4){
+        document.getElementById("content").style.display="none";
+        document.getElementById("finalscr").style.display="block";
+        document.getElementById("finalscr").innerHTML="The Final Score is "+y;
     }
 }
